5cb9bc3a74914e880e695c7aa2901fcb3c2299cf
duxiangwei <793886211@qq.com>
2019-08-26 11:08:39 +0800

    初始化项目

--- WXServer/settings.py
+++ WXServer/settings.py
@@ -74,13 +74,28 @@ WSGI_APPLICATION = 'WXServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if "duxiangwei" in BASE_DIR:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'wxserver',
+            'USER': 'root',
+            'PASSWORD': '123',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'wxserver',
+            'USER': 'root',
+            'PASSWORD': 'Aa!@123456',
+            'HOST': '127.0.01',
+            'PORT': '3306',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
