c644259c71b6297db89a91b4b7ad83ab5d8b6fc4
duxiangwei <793886211@qq.com>
2019-08-27 15:17:00 +0800

    初始化项目

--- server-start.sh
+++ server-start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 # 开启https服务
 # python3 manage.py runserver_plus --cert server.crt 8080
-python3 manage.py runserver 127.0.0.1:8081
+#python3 manage.py runserver 127.0.0.1:8081
+
+# uwsgi3
+uwsgi3 --ini uwsgi.ini
