0a92fa9a828ac7a02a9659025db877f5a3d4de0d
duxiangwei <793886211@qq.com>
2019-08-28 14:15:52 +0800

    尝试修复static报错的问题

--- WXServer/settings.py
+++ WXServer/settings.py
@@ -128,18 +128,12 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-# STATIC_URL = '/static/',
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static/')
-# ]
+STATIC_URL = '/static/',
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/')
+]
 
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-
-)
 
 
 # Static files (CSS, JavaScript, Images)
