aacb6a9ab1651502bda08ee759a5465ef4b8afd6
CountrySmart <793886211@qq.com>
2019-08-31 23:15:18 +0800

    add getIndexNavSectionData()

--- Frame/models.py
+++ Frame/models.py
@@ -1,76 +1,41 @@
 from django.db import models
 
-
-class User(models.Model):
-    """用户信息表"""
-    # 用户名
-    name = models.CharField(max_length=128, unique=True)
-    # 密码
-    password = models.CharField(max_length=256)
-    email = models.EmailField(unique=True)
-    sex = models.CharField(max_length=32, default="男")
-    c_time = models.DateTimeField(auto_now_add=True)
-    # 微信昵称
-    # 账户余额
-    # 备注
+from django.db import models
 
 
-class Brand(models.Model):
-    """品牌信息"""
-    # 品牌全名
-    name = models.CharField(max_length=1024)
-    # 品牌简称
-    short_name = models.CharField(max_length=1024, blank=True, null=True)
+#
+class Companys(models.Model):
+    addr = models.CharField(max_length=1024, null=False)
 
 
-class Staff(models.Model):
-    """员工信息"""
-    # 员工姓名
-    name = models.CharField(max_length=1024)
-    # 电话
-    phone = models.CharField(max_length=64)
-    # 性别
-    # 工作经验
-    # 特色作品
-    # 个人照片
-    # 身份证照片
+# 技师 数据
+class SkilledData(models.Model):
+    company_id = models.CharField(max_length=32, null=False)
+    avatar = models.CharField(max_length=1024, null=False, default="")
+    name = models.CharField(max_length=128, null=False)
+    nickname = models.CharField(max_length=128, null=False)
+    phone = models.CharField(max_length=32, null=False)
+    price = models.CharField(max_length=32, null=False)
+    message = models.CharField(max_length=1024, null=True, default="")
+    distance = models.CharField(max_length=128, null=True, default="")
 
 
-class Store(models.Model):
-    """店铺信息"""
-    # 品牌ID
-    brand_id = models.CharField(max_length=128)
-    # 店铺名称
-    name = models.CharField(max_length=1024)
-    # 地理位置
-    position = models.CharField(max_length=1024)
-    # 店长
-    # 员工
+# 用户数据
+class userData(models.Model):
+    avatar = models.CharField(max_length=1024, null=False, default="")
+    name = models.CharField(max_length=128, null=False)
+    nickname = models.CharField(max_length=128, null=False)
+    phone = models.CharField(max_length=32, null=False)
+    addrs = models.CharField(max_length=2048, null=True, default="")
 
 
-class Goods(models.Model):
-    """单点商品信息"""
-    # 商品名称
-    name = models.CharField(max_length=1024)
-    # 制作者id，记录Staff表内人员id
-    producer_id = models.CharField(max_length=1024)
-    # 原价
-    # 现价
-    # 内含货物
-    # 售卖状态
-    # 是否套餐
-
-
-class Order(models.Model):
-    """订单详情"""
-    # 购买者
-    # 购买商品
-    # 服务者
-    # 入账金额
-    # 订单状态
-    # 备注
-
-
-class Customer(models.Model):
-    """顾客信息"""
-    pass
+# 首页对应标签数据项
+class IndexNavSectionData(models.Model):
+    company_id = models.CharField(max_length=32, null=False)
+    subject = models.CharField(max_length=128, null=False)
+    coverpath = models.CharField(max_length=1024, null=False)
+    price = models.CharField(max_length=32, null=False)
+    message = models.CharField(max_length=1024, null=True, default="")
+    artype = models.CharField(max_length=128, null=False)
+    skilled_id = models.CharField(max_length=128, null=False)
+    is_sale = models.BooleanField(default=True)
--- meiye/views/customer.py
+++ meiye/views/customer.py
@@ -9,8 +9,93 @@ import os
 import configparser
 
 
-def index(request):
-    return render(request, "customer-index.html")
+def getIndexNavSectionData(request):
+    res = json.dumps(
+        [
+            [
+                {
+
+                    "subject": "秋季自然特价美甲",
+                    "coverpath": "../../images/recommend_img_01.png",
+                    "price": '¥198',
+                    "message": '我们追求的是没有最长只有更长！'
+                },
+                {
+
+                    "subject": "睫毛稀疏 种睫毛来帮忙",
+                    "coverpath": "../../images/recommend_img_02.png",
+                    "price": '¥1888',
+                    "message": '我们追求的是没有最长只有更长！'
+                },
+                {
+
+                    "subject": "爱丽克EircParisSalon",
+                    "coverpath": "../../images/recommend_img_03.png",
+                    "price": '¥1588',
+                    "message": '我们追求的是没有最长只有更长！'
+                },
+                {
+
+                    "subject": "伊本造型",
+                    "coverpath": "../../images/recommend_img_05.png",
+                    "price": '¥198',
+                    "message": '伊本造型是由著名形象设计师杨进先生创办。'
+                },
+                {
+
+                    "subject": " 画对了妆，微微一笑颜值倍儿高！",
+                    "coverpath": "../../images/recommend_img_06.png",
+                    "price": '¥198',
+                    "message": '《微微一笑很倾城》的剧照简直美腻到不要不要的'
+                }
+            ],
+            [
+                {
+                    "artype": 'nails',
+                    "subject": "秋季自然特价美甲",
+                    "coverpath": "../../images/recommend_img_01.png",
+                    "price": '¥198',
+                    "message": '我们追求的是没有最长只有更长！'
+                }
+            ],
+            [
+                {
+                    "artype": 'beauty',
+                    "subject": "爱丽克EircParisSalon",
+                    "coverpath": "../../images/recommend_img_03.png",
+                    "price": '¥1588',
+                    "message": '我们追求的是没有最长只有更长！'
+                },
+                {
+                    "artype": 'beauty',
+                    "subject": " 画对了妆，微微一笑颜值倍儿高！",
+                    "coverpath": "../../images/recommend_img_06.png",
+                    "price": '¥198',
+                    "message": '《微微一笑很倾城》的剧照简直美腻到不要不要的'
+                }
+            ],
+            [
+
+                {
+                    "artype": 'hair',
+                    "subject": "伊本造型",
+                    "coverpath": "../../images/recommend_img_05.png",
+                    "price": '¥1588',
+                    "message": '伊本造型是由著名形象设计师杨进先生创办。'
+                }
+            ],
+            [
+                {
+                    "artype": 'eyelash',
+                    "subject": "睫毛稀疏 种睫毛来帮忙",
+                    "coverpath": "../../images/recommend_img_02.png",
+                    "price": '¥1888',
+                    "message": '我们追求的是没有最长只有更长！'
+                }
+            ]
+        ]
+    )
+    return HttpResponse(res)
 
 
 def goods(request):
